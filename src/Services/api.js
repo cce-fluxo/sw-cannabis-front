8c15fc49491d38b06937b1cf4a5bd9dc7e9e2126
iggodinho <igorgt30@gmail.com>
2021-10-13 15:31:02 -0300

    change to yarn

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "https://sw-cannabis-master.herokuapp.com/",
+  baseURL: "https://sw-cannabis-back-orvnl.ondigitalocean.app/",
   
 });
 
